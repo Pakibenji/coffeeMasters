bd40edf30b061d571d1b33dc286d669b1efb5f6c
Benjamin Alcade <alcade.benjamin@gmail.com>
2023-10-02 15:30:24 +0200

    router done

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import Store from "./services/Store.js";
 import { loadData } from "./services/Menu.js";
+import Router from "./services/Router.js";
 
 window.app = {};
 app.store = Store;
+app.router = Router
 
 window.addEventListener("DOMContentLoaded", async () => {
     loadData();
+    app.router.init()
 });
---
+++ services/Router.js
@@ -0,0 +1,49 @@
+const Router = {
+  init: () => {
+    document.querySelectorAll("a.navlink").forEach((a) => {
+      a.addEventListener("click", (event) => {
+        event.preventDefault();
+        // const url1 = event.target.href
+        const url = event.target.getAttribute("href");
+        Router.go(url);
+      });
+    });
+
+    window.addEventListener("popstate", event => {
+        Router.go(event.state.route, false)
+    })
+    Router.go(location.pathname);
+  },
+  go: (route, addToHistory = true) => {
+    console.log(`going top ${route}`);
+    if (addToHistory) {
+      history.pushState({ route }, "", route);
+    }
+    let pageElement = null;
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Menu";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Your Order";
+        break;
+       default: 
+       if (route.startsWith("/product-")) {
+            pageElement = document.createElement("h1")
+            pageElement.textContent = "Details";
+            const paramId = route.substring(route.lastIndexOf("-") +1 )
+            pageElement.dataset.id = paramId;  
+        }
+    }  
+
+    const cache = document.querySelector("main");
+    cache.innerHTML = "";
+    cache.appendChild(pageElement);
+    window.scrollX = 0;
+    window.scrollY = 0;
+  },
+};
+
+export default Router;
